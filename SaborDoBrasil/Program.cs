b9152c622029e620fe912c77bb2349fe7323bebf
Theo Quintão <0000900198@senaimgaluno.com.br>
2025-05-07 14:01:08 -0300

    alterando tipo de projeto

--- SaborDoBrasil/Program.cs
+++ SaborDoBrasil/Program.cs
@@ -1,13 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.UseDefaultFiles();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.MapGet("/", async context =>
-{
-await context.Response.SendFileAsync("wwwroot/index.html");
-});
+app.UseRouting();
 
+app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.Run();
